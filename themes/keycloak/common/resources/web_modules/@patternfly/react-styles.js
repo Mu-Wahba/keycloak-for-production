a77ebfb941f30246052167854e120d99aa3ea7bb
Muhammad Wahba <eng.muhammad.w110@gmail.com>
2020-10-21 07:55:45 +0000

    default keycloak package

---
+++ themes/keycloak/common/resources/web_modules/@patternfly/react-styles.js
@@ -0,0 +1,2 @@
+import"../common/_commonjsHelpers-a61ae4b4.js";export{S as StyleSheet,c as css,d as getClassName,b as getInsertedStyles,g as getModifier,a as isModifier,i as isValidStyleDeclaration,p as pickProperties}from"../common/StyleSheet-c3a1e2a3.js";
+//# sourceMappingURL=react-styles.js.map
